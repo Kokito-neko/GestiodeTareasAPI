e855d694680c1fd22aaeabe3b7ef743eb3803014
Tu Nombre <alms00020130@gmail.com>
2026-06-26 19:06:05 -0400

    chore: resolver conflicto de fusión en CreateTaskDto

--- Modules/Tasks/Dtos/CreateTaskDto.cs
+++ Modules/Tasks/Dtos/CreateTaskDto.cs
@@ -1,27 +1,18 @@
-<<<<<<< HEAD
-namespace TaskManagerApi.Modules.Tasks.Dtos;
-
-public class CreateTaskDto 
-{
-    public required string Title { get; set; }
-    public required string Description { get; set; }
-=======
 using System.ComponentModel.DataAnnotations;
 
-namespace WebApiLayerArchitecture.Modules.Tasks.Dtos;
+namespace TaskManagerApi.Modules.Tasks.Dtos;
 
 public class CreateTaskDto
 {
-    [Required(ErrorMessage = "El título es requerido")]
+    [Required(ErrorMessage = "El título es obligatorio")]
     [MinLength(3, ErrorMessage = "El título debe tener mínimo 3 caracteres")]
     public required string Title { get; set; }
 
-    [Required(ErrorMessage = "La descripción es requerida")]
+    [Required(ErrorMessage = "La descripción es obligatoria")]
     [MinLength(3, ErrorMessage = "La descripción debe tener mínimo 3 caracteres")]
     public required string Description { get; set; }
 
-    [Required(ErrorMessage = "La prioridad es requerida")]
+    [Required(ErrorMessage = "La prioridad es obligatoria")]
     [MinLength(3, ErrorMessage = "La prioridad debe tener mínimo 3 caracteres")]
->>>>>>> 9da3acfb0156b05053fb0248630aa920938efa06
-    public required string Priority { get; set; }
+    public required string Priority { get; set; } // Ejemplo: Alta, Media, Baja
 }
