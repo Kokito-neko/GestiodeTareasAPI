23d214b31b50f5337e949a8e5cecc97a3735900d
Tu Nombre <alms00020130@gmail.com>
2026-06-26 19:02:41 -0400

    chore: resolver conflicto final en UpdateTaskDto con validaciones

--- Modules/Tasks/Dtos/UpdateTaskDto.cs
+++ Modules/Tasks/Dtos/UpdateTaskDto.cs
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
+using System.ComponentModel.DataAnnotations;
+
 namespace TaskManagerApi.Modules.Tasks.Dtos;
 
 public class UpdateTaskDto 
 {
-    public string? Title { get; set; }
-    public string? Description { get; set; }
-=======
-using System.ComponentModel.DataAnnotations;
-
-namespace WebApiLayerArchitecture.Modules.Tasks.Dtos;
-
-public class UpdateTaskDto
-{
-    // Al usar el signo '?' permitimos que los campos sean opcionales (para peticiones PATCH)
     [MinLength(3, ErrorMessage = "El título debe tener mínimo 3 caracteres")]
     public string? Title { get; set; }
 
@@ -20,6 +11,5 @@ public class UpdateTaskDto
     public string? Description { get; set; }
 
     [MinLength(3, ErrorMessage = "La prioridad debe tener mínimo 3 caracteres")]
->>>>>>> 9da3acfb0156b05053fb0248630aa920938efa06
     public string? Priority { get; set; }
 }
--- Modules/Tasks/Entities/TodoTask.cs
+++ Modules/Tasks/Entities/TodoTask.cs
@@ -1,26 +1,13 @@
-<<<<<<< HEAD
 using System.ComponentModel.DataAnnotations;
 
 namespace TaskManagerApi.Modules.Tasks.Entities;
 
 public class TodoTask 
 {
-    [Key] // es una etiqueta, le avisa a EF, que la clumna id e la llave primaria 
-    public int Id { get; set; } // el indicador sera de numero entero
-    public required string Title { get; set; }
-    public required string Description { get; set; }
-    public required string Priority { get; set; }
-=======
-namespace WebApiLayerArchitecture.Modules.Tasks.Entities;
+    [Key] // Es una etiqueta, le avisa a EF que la columna id es la llave primaria 
+    public int Id { get; set; } // El identificador será de número entero
 
-public class TodoTask
-{
-    // Genera automáticamente un ID único en formato UUID para cada tarea
-    public Guid Uuid { get; } = Guid.NewGuid();
-    
-    // Las propiedades obligatorias que definen a nuestra tarea
     public required string Title { get; set; }
     public required string Description { get; set; }
     public required string Priority { get; set; } // Ejemplo: Alta, Media, Baja
->>>>>>> 9da3acfb0156b05053fb0248630aa920938efa06
 }
